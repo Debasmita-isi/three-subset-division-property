4edf31f9855c7560b55e8b4b97543501e68f226f
Debasmita Chakraborty <77646021+Debasmita-isi@users.noreply.github.com>
2024-07-19 02:13:31 +0530

    Update main.py

--- MANTIS/1-subset/main.py
+++ MANTIS/1-subset/main.py
@@ -1,10 +1,4 @@
-# Algorithm 3 presented in paper "Applyint MILP Method to Searching Integral 
-# Distinguishers based on Division Property for 6 Lightweight Block Ciphers"
-# Regarding to the paper, please refer to https://eprint.iacr.org/2016/857
-# For more information, feedback or questions, pleast contact at xiangzejun@iie.ac.cn
 
-# Implemented by Xiang Zejun, State Key Laboratory of Information Security, 
-# Institute Of Information Engineering, CAS
 
 from mantis import Mantis
 
